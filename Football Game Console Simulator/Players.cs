3e9bb8b8f5102f4838d669026fefbf611b1b7c22
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-09 12:03:37 +0400

    create player class ,attributes,Constructor

---
+++ Football Game Console Simulator/Players.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Football_Game_Console_Simulator.Players;
+
+namespace Football_Game_Console_Simulator
+{
+    public class Players
+    {
+        public string names;
+        public enum Position
+        {
+            Forward, Midfielder, Defender
+        }
+        public Position position;
+        public int Skill;
+        public Players(string names, Position position, int Skill)
+        {
+            this.names = names;
+            this.position = position;
+            this.Skill = Skill;
+        }
+    }
+ 
+}
