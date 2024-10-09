2012f74c8e11734e13790ac5a56b26d0ce88d94e
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-09 12:17:40 +0400

    create Game class ,attributes,Constructor

---
+++ Football Game Console Simulator/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football_Game_Console_Simulator
+{
+    public class Game
+    {
+        public string team1;
+        public string team2;
+        public int score;
+        public enum Posititon
+        {
+            Attack,Defender
+        }
+        public Posititon posititon { get; set; }
+        public string Half;
+
+
+    }
+}
