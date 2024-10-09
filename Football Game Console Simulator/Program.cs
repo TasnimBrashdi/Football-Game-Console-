b4623ee2eaa24f68fdc45b3ebfeda640753ea40a
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-09 11:55:51 +0400

    first commit

---
+++ Football Game Console Simulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Football_Game_Console_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
