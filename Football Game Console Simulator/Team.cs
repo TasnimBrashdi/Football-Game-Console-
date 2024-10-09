b0f69b489fc8530d89317f10b49839962f1bc5d6
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-09 12:12:30 +0400

    create Team class ,attributes,Constructor

---
+++ Football Game Console Simulator/Team.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football_Game_Console_Simulator
+{
+    public class Team
+    {
+        public string NameTeam { get; set; }
+        public List<Players> Teams { get; set; }
+        public Team(string NameTeam) { 
+        this.NameTeam = NameTeam;   
+        this.Teams = new List<Players>();
+        
+        
+        }
+    }
+}
