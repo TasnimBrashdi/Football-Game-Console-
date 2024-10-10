2b5e70524ffe8d0b03c152317df7050b35271082
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-10 09:49:55 +0400

    Edit method on class teams and adedd new

--- Football Game Console Simulator/Players.cs
+++ Football Game Console Simulator/Players.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using static Football_Game_Console_Simulator.Players;
@@ -9,17 +10,51 @@ namespace Football_Game_Console_Simulator
 {
     public class Players
     {
-        public string[] players= { "P1","P2","P3","P4","P5","P6","P7","P8","P9","P10","P11","P12","P13","P14","P15","P16","P17","P18","P19","P20","P21"};
-       
-        public int Skill;
-        public Players()
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Skill { get; set; }
+
+        public Players(string name, string position, int skill)
         {
-            this.players = players;
-     
+            Name = name;
+            Position = position;
+            Skill = skill;
         }
-        
-       
     }
+
+    public class Player
+    {
+        public List<Players> playerList;
+
+        public Player()
+        {
+            playerList = new List<Players>
+        {
+            new Players("P1", "Forward", 0),
+            new Players("P2", "Midfielder", 0),
+            new Players("P3", "Defender", 0),
+            new Players("P4", "Midfielder", 0),
+            new Players("P5", "Forward", 0),
+            new Players("P6", "Midfielder", 0),
+            new Players("P7", "Defender", 0),
+            new Players("P8", "Goalkeeper", 0),
+            new Players("P9", "Forward", 0),
+            new Players("P10", "Forward", 0),
+            new Players("P11", "Defender", 0),
+            new Players("P12", "Defender", 0), 
+            new Players("P13", "Midfielder", 0),
+            new Players("P14", "Defender", 0),
+            new Players("P15", "Goalkeeper", 0),
+            new Players("P16", "Forward", 0),
+            new Players("P17", "Midfielder", 0),
+            new Players("P18", "Defender", 0),
+            new Players("P19", "Midfielder", 0),
+            new Players("P20", "Forward", 0),
+            new Players("P21", "Midfielder", 0),
+  
+        };
+        }
     }
+}
     
 
--- Football Game Console Simulator/Team.cs
+++ Football Game Console Simulator/Team.cs
@@ -9,79 +9,41 @@ namespace Football_Game_Console_Simulator
 {
     public class Team
     {
-        public string NameTeam { get; set; }
-        public List<string> Team1 { get; set; }
-        public List<string> Team2 { get; set; }
-        public int Skill;
+        public string Name { get; set; }
+        public List<Players> Players { get; set; } = new List<Players>();
 
-        public enum Position
+        public Team(string name)
         {
-            Forward, Midfielder, Defender
+            Name = name;
         }
 
-        public Dictionary<string, Position> PlayerPositions { get; set; }
-
-        public Team(string nameTeam)
+        public void AssignPlayers(Player playerPool)
         {
-            this.NameTeam = nameTeam;
-            this.Team1 = new List<string>();
-            this.Team2 = new List<string>();
-            this.PlayerPositions = new Dictionary<string, Position>();
-        }
-
-        public void AddPlayersToTeams(Players playerList)
-        {
-            List<string> allPlayers = new List<string>(playerList.players);
             Random random = new Random();
+            var allPlayers = playerPool.playerList;
+            allPlayers.Shuffle(random); 
 
-            while (allPlayers.Count > 0)
+            for (int i = 0; i < 11; i++) 
             {
-                int randomIndex = random.Next(allPlayers.Count);
-                string selectedPlayer = allPlayers[randomIndex];
-                allPlayers.RemoveAt(randomIndex);
-
-                if (Team1.Count <= Team2.Count)
-                {
-                    Team1.Add(selectedPlayer);
-                }
-                else
-                {
-                    Team2.Add(selectedPlayer);
-                }
+                Players.Add(allPlayers[i]);
             }
 
+        
+            foreach (var player in Players)
+            {
+                player.Skill = random.Next(1, 101);
+            }
         }
 
-
-        //public void RandomPosition()
-        //{
-        //    Random random = new Random();
-
-        //    foreach (var player in Team1)
-        //    {
-        //        Position randomPosition = (Position)random.Next(Enum.GetNames(typeof(Position)).Length);
-        //        PlayerPositions[player] = randomPosition; 
-        //    }
-
-        //    foreach (var p in PlayerPositions)
-        //    {
-        //        Console.WriteLine($"{p.Key}: {p.Value}");
-        //    }
-        //}
-        public void DisplayTeams()
+        public void PrintPlayers()
         {
-            Console.WriteLine($"Team: {NameTeam}");
-            Console.WriteLine("\nTeam 1 Players:");
-            foreach (var player in Team1)
+            Console.WriteLine($"{Name} Players:");
+            foreach (var player in Players)
             {
-                Console.WriteLine(player);
-            }
-
-            Console.WriteLine("\nTeam 2 Players:");
-            foreach (var player in Team2)
-            {
-                Console.WriteLine(player);
+                Console.WriteLine($"{player.Name} - {player.Position} - Skill: {player.Skill}");
             }
         }
     }
 }
+    
+
